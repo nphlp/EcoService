fee16438e16dc22861e6d2b056eb0aef9bf1b397
nansphilip <nansphilip@gmail.com>
2025-03-12 14:51:41 +0100

    fix: rm failed cached files

--- actions/(examples)/FileExists.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import fs from "fs";
-import path from "path";
-
-export const FileExists = async (filePath: string | null, folderName: string): Promise<boolean> => {
-    const absolutePath = path.join(process.cwd(), folderName, String(filePath));
-    return fs.existsSync(path.resolve(absolutePath));
-};
--- prisma/sql/reset.sql
+++
@@ -1,5 +0,0 @@
--- Deletes the database user
-DROP USER IF EXISTS 'eco-service-user'@'localhost';
-
--- Delete the database
-DROP DATABASE IF EXISTS `eco-service-db`;
