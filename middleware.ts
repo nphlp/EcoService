b1d4361e57661d01f7a3f929c31cbd546c003591
nansphilip <nansphilip@gmail.com>
2025-07-11 17:15:59 +0200

    feat: update deps

--- middleware.ts
+++ middleware.ts
@@ -5,12 +5,15 @@ const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 if (!baseUrl) throw new Error("NEXT_PUBLIC_BASE_URL environment variable is not defined");
 
 export async function middleware(request: NextRequest) {
-    const isAuthorized = await analyseRequest(request);
+    // Skip in dev for faster refresh
+    // if (process.env.NODE_ENV === "development") return NextResponse.next();
 
-    if (!isAuthorized) {
-        // return NextResponse.next(); // WARNING: To disable temporarily
-        return NextResponse.redirect(`${baseUrl}/unauthorized`);
-    }
+    // Skip in test to dodge permissions
+    if (process.env.NODE_ENV === "test") return NextResponse.next();
+
+    // Analyse request
+    const isAuthorized = await analyseRequest(request);
+    if (!isAuthorized) return NextResponse.redirect(`${baseUrl}/unauthorized`);
 
     return NextResponse.next();
 }
