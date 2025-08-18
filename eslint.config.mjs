37248f94335fbbd1e22bee8057f648a7126d8b9c
nansphilip <nansphilip@gmail.com>
2025-08-18 11:45:55 +0200

    fix: upgrade config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,6 @@
 import { FlatCompat } from "@eslint/eslintrc";
+import reactRefresh from "eslint-plugin-react-refresh";
+import unusedImports from "eslint-plugin-unused-imports";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -9,6 +11,19 @@ const compat = new FlatCompat({
     baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
+const eslintConfig = [
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        plugins: {
+            "react-refresh": reactRefresh,
+            "unused-imports": unusedImports,
+        },
+        rules: {
+            "react-refresh/only-export-components": "warn",
+            "unused-imports/no-unused-imports": "error",
+            "unused-imports/no-unused-vars": "warn",
+        },
+    },
+];
 
 export default eslintConfig;
