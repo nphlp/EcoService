29f3850c1b68f353ffb8f2a0e8efcdaaa8a8db79
nansphilip <nansphilip@gmail.com>
2026-01-26 12:34:43 +0100

    fix: rm location api route

--- app/api/Routes.ts
+++ app/api/Routes.ts
@@ -1,5 +1,4 @@
 import { HealthResponse } from "./health/route";
-import { LocationProps, LocationResponse } from "./location/route";
 import { type Routes as SolidRoutes } from "./solid/Routes";
 import { type Routes as StripeRoutes } from "./stripe/Routes";
 
@@ -8,11 +7,5 @@ type SubRoutes<Input> = StripeRoutes & SolidRoutes<Input>;
 export type Routes<Input> = SubRoutes<Input> & {
     "/health": () => {
         response: HealthResponse;
-    };
-
-    "/location": () => {
-        method: "GET";
-        params: LocationProps;
-        response: LocationResponse;
     };
 };
